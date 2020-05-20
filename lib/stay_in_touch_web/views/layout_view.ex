8960a8b63023f1146780fa0eb1d45c1e9bd83d46
Vinícius Bertoldi <bertoldi11@gmail.com>
2020-05-20 20:12:07 -0300

    Using gravatar to handle avatar image while dont find a definitive solution to save images

--- lib/stay_in_touch_web/views/layout_view.ex
+++ lib/stay_in_touch_web/views/layout_view.ex
@@ -1,7 +1,25 @@
 defmodule StayInTouchWeb.LayoutView do
   use StayInTouchWeb, :view
 
+  @default_gravatar "https://saudeemeulugar.com/avatar/319"
+
   def get_user_id(%{assigns: %{current_user: current_user}}) do
     current_user.id
+  end
+
+  def get_gravatar_url(email, size \\ 200) do
+    "https://www.gravatar.com/avatar/"
+      <> get_gravatar_hash(email)
+      <> "?s=#{size}"
+      <> "&d=#{URI.encode(@default_gravatar)}"
+  end
+
+  def get_gravatar_hash(email) do
+    email = email
+    |> String.trim
+    |> String.downcase
+
+    :crypto.hash(:md5, email)
+    |> Base.encode16(case: :lower)
   end
 end
